60200ac7ba20bddc2554d04169b2fa9e5e13a29e
m4rkovic <46758911+m4rkovic@users.noreply.github.com>
2026-09-08 18:51:30 +0200

    Complete Serbian copy proofing

---
+++ assets/copy-final.js
@@ -0,0 +1,67 @@
+(() => {
+  'use strict';
+
+  const translations = window.translations;
+  if (!translations) return;
+
+  Object.assign(translations.en || {}, {
+    value_fruit_list: 'Sour cherry, raspberry, strawberry, blackberry, blueberry, plum'
+  });
+
+  Object.assign(translations.sr || {}, {
+    why_title: 'Partner u snabdevanju prilagođen <span class="font-serif italic font-normal text-brand-700">potrebama prehrambene industrije</span>',
+    why_desc: 'Od lokalne nabavke voća i tople i hladne prerade do međunarodne usklađenosti i formulacija po meri, naše poslovanje organizovano je za pouzdano B2B snabdevanje.',
+    why1_title: '30 godina prerade',
+    why1_desc: 'Porodično preduzeće sa iskustvom u preradi voća od 1996. godine i razvijenom proizvodnjom u Srbiji.',
+    why2_title: 'Blizina sirovina',
+    why2_desc: 'Proizvodnja na području Kuršumlije omogućava direktan pristup značajnim voćarskim regionima Srbije.',
+    why3_title: 'Topla i hladna prerada',
+    why3_desc: 'Širok spektar procesa omogućava proizvodnju smrznutih, termostabilnih i drugih voćnih formata po meri kupca.',
+    why4_title: 'Međunarodna tržišta',
+    why4_desc: 'Iskustvo izvoza u Evropu, Severnu Ameriku i na Bliski istok, uz primenu međunarodnih standarda.',
+    catalogue_back: 'Nazad na Frigonais',
+    catalogue_eyebrow: 'Kompletan katalog',
+    catalogue_title: 'Sve kategorije proizvoda Frigonais',
+    catalogue_desc: 'Tehničke specifikacije, uzorci i razgovori o formulacijama po meri dostupni su preko našeg komercijalnog tima.',
+    products_page_title: 'Voćni sastojci za industrijsku proizvodnju hrane',
+    modal_label: 'Informacije o proizvodu',
+    modal_note: 'Tehničke specifikacije, uzorke i detalje formulacije komercijalni tim dostavlja prema zahtevima konkretne primene.',
+    modal_quote: 'Zatražite specifikacije / ponudu',
+    modal_continue: 'Nastavite pregledanje',
+    value_cold_iqf: 'Hladna prerada / IQF',
+    value_fruit_list: 'Višnja, malina, jagoda, kupina, borovnica, šljiva',
+    value_bakery_dairy_retail: 'Pekarska i mlečna industrija, maloprodaja',
+    value_beverage_dairy_conf: 'Industrija pića, mlečna i konditorska industrija',
+    value_single_blended: 'Jedna vrsta voća i mešavine',
+    value_pastries: 'Peciva, kroasani, krofne i konditorski proizvodi',
+    form_security: 'Zaštićeno sistemom Cloudflare Turnstile.',
+    production_email: 'E-pošta proizvodnje',
+    core_iqf: 'IQF voće',
+    core_purees: 'Voćni pirei',
+    core_thermostable: 'Termostabilna punjenja',
+    core_custom: 'Formulacije po meri',
+    industrial_title: 'Za industrijske primene',
+    industrial_desc: 'Tehničke specifikacije, uzorci i razgovori o formulacijama po meri dostupni su preko našeg komercijalnog tima.',
+    industrial_cta: 'Razgovarajte o primeni',
+    metric_markets: 'Izvozna tržišta',
+    metric_markets_desc: 'Uspostavljeno međunarodno B2B prisustvo',
+    metric_continents: 'Kontinenti na kojima poslujemo',
+    metric_continents_desc: 'Evropa, Severna Amerika i Bliski istok',
+    metric_exports: 'Godišnji izvoz',
+    metric_exports_desc: 'Iskustvo u međunarodnom snabdevanju prehrambene industrije',
+    trade_presence: 'Međunarodna prodaja i prisustvo na tržištu',
+    region_europe_desc: 'Uspostavljena izvozna tržišta',
+    region_na_desc: 'B2B snabdevanje voćnim sastojcima',
+    region_me_desc: 'Rastuće prisustvo na tržištu',
+    catalogue_cta_label: 'Potrebna vam je formulacija ili specifikacija?',
+    catalogue_cta_title: 'Razgovarajte direktno sa komercijalnim timom Frigonais.'
+  });
+
+  Object.assign(translations.zh || {}, {
+    value_fruit_list: '酸樱桃、覆盆子、草莓、黑莓、蓝莓、李子'
+  });
+
+  Object.assign(translations.ar || {}, {
+    value_fruit_list: 'كرز حامض، توت أحمر، فراولة، توت أسود، توت أزرق، برقوق'
+  });
+})();
